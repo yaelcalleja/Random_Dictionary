d7135265939f656f277894c6432d89511d9dcab6
badger <yael.calleja321@gmail.com>
2025-10-25 18:55:50 -0600

    Datos para el archivo main.py

---
+++ semillas.py
@@ -0,0 +1,95 @@
+
+# Diccionario que contiene todas las carreras
+CarrerasCUGdl = {
+    "IA001" : "IA y ciencia de datos",
+    "CS101" : "Ciberseguridad",
+    "CD101" : "Creatividad digital",
+    "IF101" : "Inteligencia financiera",
+    "BM101" : "Tecnologías biomédicas"
+}
+print(type(CarrerasCUGdl))
+# Diccionario que contiene "todas" las materias
+MateriasGenerales = {
+    "MT101":  "Matematicas I",
+    "MT202":  "Algebra lineal",
+    "IN101":  "Introducción a la ciencia de datos",
+    "PC101":  "Pensamiento crítico",
+    "IC101":  "Inteligencia colectiva",
+    "IT101":  "Innovación tecnológica",
+    "PG101":  "Problemas globales",
+    "MT102":  "Matemáticas II",
+    "PR101":  "Programacion I",
+    "MT201":  "Probabilidad y estadística",
+    "ST101":  "Storytelling",
+    "ER101":  "Ética y responsabilidad social",
+    "GP101":  "Gestión de proyectos",
+    "EM001":  "Gestión socioemocional",
+    "TS001":  "Tópicos selectos en asuntos sin importancia",
+    "PR103": "Programacion II",
+    "AV003": "Analizis y visualizacion de datos para Ciberseguridad",
+    "HE003": "Hacking Etico (Fundamentos)",
+    "SO003": "Sistemas OperativosDistrinuidos",
+    "FR003": "Fundamentos de Redes y Telecomunicaciones",
+    "GR003": "Gestion de Riesgos y Recuperacion de Desastres",
+    "NC003": "La nube computacional y la Ciberseguridad",
+    "BD003": "Bases de datos"
+}
+
+# Tupla que contiene todos los semestres
+TuplaSemestres = (1, 2, 3)
+
+# Tupla que contiene las materias de primer semestre
+MateriasSemestre1 = (
+    "MT101",
+    "MT202",
+    "IN101",
+    "PC101",
+    "IC101",
+    "IT101",
+    "PG101",
+)
+
+# Tupla que contiene las materias de segundo semestre
+MateriasSemestre2 = (
+    "MT102",
+    "PR101",
+    "MT201",
+    "ST101",
+    "ER101",
+    "GP101",
+    "EM001"
+)
+
+# Tupla que contiene las materias de tercer semestre
+MateriasSemestre3 = (
+    "PR103",
+    "AV003",
+    "HE003",
+    "SO003",
+    "FR003",
+    "GR003",
+    "NC003",
+    "BD003"
+)
+# Arreglo que contiene los nombres
+ArrayNombres = [
+    "Jose",
+    "Juan",
+    "Sofia",
+    "Lorena",
+    "Socorro"
+]
+
+# Arreglo que contiene los apellidos
+ArrayApellidos = [
+    "Hernandez",
+    "Gutierrez",
+    "López",
+    "Sheinbaum",
+    "Cervantes"
+]
+
+
+
+
+
